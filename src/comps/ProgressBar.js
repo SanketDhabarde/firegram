b1f1987e6a911761d22c467b7d8805a5917d029d
SanketDhabarde <sanketdhabarde13@gmail.com>
2021-04-14 17:55:22 +0530

    feat: add progress bar

---
+++ src/comps/ProgressBar.js
@@ -0,0 +1,18 @@
+import React , { useEffect } from 'react';
+import useStorage from '../hooks/useStorage';
+
+const ProgressBar = ({file, setFile}) => {
+    const { progress, url} = useStorage(file);
+    console.log(progress, url);
+    useEffect(() => {
+        if(url){
+            setFile(null);
+        }
+    }, [url]);
+
+    return(
+        <div className="progress-bar" style={{width: progress + '%'}}></div>
+    )
+}
+
+export default ProgressBar;
--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ProgressBar from './ProgressBar';
 
 const UploadForm = () => {
     const [file, setFile] = useState(null);
@@ -20,10 +21,14 @@ const UploadForm = () => {
 
     return(
         <form>
-            <input type="file" onChange={changeHandler}/>
+            <label>
+                <input type="file" onChange={changeHandler}/>
+                <span>+</span>
+            </label>
             <div className="output">
                 {error && <div className="error">{error}</div>}
                 {file && <div>{file.name}</div>}
+                {file && <ProgressBar file={file} setFile={setFile}/>}
             </div>
         </form>
     );
